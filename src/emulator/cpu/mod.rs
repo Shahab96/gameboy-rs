e3211d03d958868d76fd6771467750fbed5a33c0
Shahab Dogar <shahab@dogar.dev>
2023-07-08 19:04:05 -0700

    use workspace

--- src/emulator/cpu/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod alu;
-pub mod context;
-pub mod cpu;
-pub mod instruction;
-pub mod register;
--- src/emulator/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod bus;
-pub mod cartridge;
-pub mod core;
-pub mod cpu;
-pub mod emulation;
-pub mod utils;
